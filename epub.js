722794b226022ecfef09be46fe831e2ef1528622
gasweld <gasweld@gmail.com>
2024-06-16 16:12:34 +0300

    Merge in another epub script

--- epub.js
+++ epub.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
 const child_process = require("child_process");
+const epub = require("/home/gas/code/pub/epub-reader/epub/epub.bin");
 
 const main = (args) => {
   if (args.length == 0) {
-    console.log("usage:\n\tepub init <dirname>\n\tepub build <dirname>");
+    console.log(
+      "usage:\n\tepub init <dirname>\n\tepub build <dirname>\n\tepub rename [-d] <filepath...>"
+    );
     return 1;
   }
 
@@ -12,6 +15,8 @@ const main = (args) => {
       return init(args.slice(1));
     case "build":
       return build(args.slice(1));
+    case "rename":
+      return rename(args.slice(1));
     default:
       console.log("unknown command: " + args[0]);
       return 1;
@@ -49,6 +54,39 @@ const build = async (args) => {
     dirname + ".epub",
   ]);
   return 0;
+};
+
+const rename = async (args) => {
+  let errors = 0;
+  let dry = false;
+  if (args[0] == "-d") {
+    dry = true;
+    args.shift();
+  }
+
+  for (const path of args) {
+    const data = fs.readFileSync(path);
+    const b = await epub.load(data);
+    const title = await b.title();
+    const author = await b.author();
+    if (!title || !author) {
+      process.stderr.write(
+        `couldn't get title or author (${author} ${title})\n`
+      );
+      errors++;
+      continue;
+    }
+    const newname = `${author} - ${title}`.replace(/\//g, "-");
+    const newpath = `${newname}.epub`;
+    if (newpath == path) {
+      process.stderr.write(`${path}: same\n`);
+      continue;
+    }
+    console.log(`${path}\n->${newpath}`);
+    if (!dry) {
+      fs.renameSync(path, newpath);
+    }
+  }
 };
 
 process.exit(main(process.argv.slice(2)));
